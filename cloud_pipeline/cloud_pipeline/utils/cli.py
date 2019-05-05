a68b2a7e19231fb8fabd2e4526c484239ff4fcdb
Wey Gu <littlewey@gmail.com>
2019-05-05 21:43:02 +0800

    Introduced get_bench subcommand for cloudp

--- cloud_pipeline/cloud_pipeline/utils/cli.py
+++ cloud_pipeline/cloud_pipeline/utils/cli.py
@@ -10,12 +10,13 @@ cli_help_string = """
 usage: cloudp
 
 Commands:
-  log            Monitoring cloud pipeline logs
-  stack          Monitoring VNF events
-  load_gen       Load generation
-  load_clean     Load cleanup
-  vnf_create     VNF create
-  vnf_clean      VNF cleanup
+  log               Monitoring cloud pipeline logs
+  stack             Monitoring VNF events
+  load_gen          Load generation
+  load_clean        Load cleanup
+  vnf_create        VNF create
+  vnf_clean         VNF cleanup
+  get_bench [id]    Get latest benchmark
 
 """
 
@@ -51,8 +52,35 @@ def main():
             cp = CloudPipeline()
             if cp.vnf_existed():
                 cp.vnf_cleanup()
+        if args[0] == "get_bench":
+            from cloud_pipeline.handler.data_collector import DataCollector
+            from cloud_pipeline.config import ARG_KEYS
+            import numpy as np
+            # generate a random argument list
+            randomeArgList = np.random.uniform(
+                low=-1.0, high=1.0, size=(len(ARG_KEYS),))
+
+            # build argument dict
+            arg = dict(zip(ARG_KEYS, randomeArgList))
+            try:
+                new_id = args[1]
+            except IndexError:
+                new_id = 65535
+                print("Warning: id missing, id:65535 mocked in this query for testing...")
+            data_collector = DataCollector(arg, ansible_stdout=True, new_id=int(new_id))
+            try:
+                print("Processing started...")
+                data_collector.parse_data()
+                print("Processing finished...\n    Benchmark: " + str(data_collector.benchmark))
+            except Exception as e:
+                print("get_bench failed: %s" % (str(e)))
+
     except Exception as e:
         cli_help()
+
+def arg_validator(args):
+    """tobedone"""
+    pass
 
 def cli_help():
     print(cli_help_string)

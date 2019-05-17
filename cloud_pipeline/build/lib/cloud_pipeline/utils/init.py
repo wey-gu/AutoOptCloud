98da670f513ca46f0b9c5010382d10a7fc5292e5
Wey Gu <littlewey@gmail.com>
2019-05-17 18:08:23 +0800

    remove build/lib

--- cloud_pipeline/build/lib/cloud_pipeline/utils/init.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-from .logger import LOG_FOLDER
-
-class Initiator():
-    def __init__(self):
-        subprocess.check_output(
-                ["mkdir", "-p", LOG_FOLDER]
-            )
